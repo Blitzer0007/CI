a0acb416c0964b19890c671039a147e514f6d361
Blitzer0007 <harishjune777@gmail.com>
2025-01-12 10:39:33 +0530

    Changes for Sonar

--- src/test/java/com/DemoProj/TestCaseUI.java
+++ src/test/java/com/DemoProj/TestCaseUI.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,18 +25,21 @@ public class TestCaseUI {
     }
 
     @Test
-    public void Test1() throws InterruptedException {
+    public void Test1() {
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         driver.get("http://demo.guru99.com/test/drag_drop.html");
+        wait.until(ExpectedConditions.urlMatches("https://demo.guru99.com/test/drag_drop.html"));
         Actions builder = new Actions(driver);
         WebElement source = driver.findElement(By.xpath("//a[normalize-space()='BANK']"));
         WebElement des = driver.findElement(By.xpath("(//li[@class='placeholder'])[1]"));
-        Thread.sleep(2000);
         builder.clickAndHold(source).moveToElement(des).release(des).perform();
     }
 
     @Test
     public void Test2() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         driver.get("http://demo.guru99.com/test/drag_drop.html");
+        wait.until(ExpectedConditions.urlMatches("https://demo.guru99.com/test/drag_drop.html"));
         Actions builder = new Actions(driver);
         WebElement source = driver.findElement(By.xpath("//a[normalize-space()='BANK']"));
         WebElement des = driver.findElement(By.xpath("(//li[@class='placeholder'])[1]"));
@@ -44,7 +49,9 @@ public class TestCaseUI {
 
     @Test
     public void Test3() {
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         driver.get("https://www.rediff.com/");
+        wait.until(ExpectedConditions.urlMatches("https://www.rediff.com/"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.findElement(By.xpath("//a[contains(@title,'Sign in')]")).click();
     }
